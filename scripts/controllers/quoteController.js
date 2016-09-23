baef0196291a73c9c8e51a6bb4d62ca8e3a8a93c
wnamen <williamjosephnamen@gmail.com>
2016-09-22 20:54:40 -0700

    consolidated search controller into this file

--- scripts/controllers/quoteController.js
+++ scripts/controllers/quoteController.js
@@ -4,15 +4,31 @@ angular.module('clarity')
 quoteController.$inject = ['$http', '$scope'];
 function quoteController ($http, $scope){
   var vm = this;
-  vm.top = 'aapl,msft,xom,jnj,amzn,fb,brk.b,ge,t,jpm,pg,goog,googl,wfc,vz,pfe,cvx,intc,mrk,ko,bac,cmcsa,v,hd,csco';
-  var url = 'http://marketdata.websol.barchart.com/getQuote.jsonp?key=25df1e65da1e4a624c404c83a2f376ec&symbols=';
+  vm.topQuotes = ['aapl','msft','xom','jnj','amzn','fb','brk.b','ge','t','jpm','pg','goog','googl','wfc','vz','pfe','cvx','intc','mrk','ko','bac','cmcsa','v','hd','csco'];
+  var url = 'http://marketdata.websol.barchart.com/getQuote.jsonp?key=25df1e65da1e4a624c404c83a2f376ec&mode=R&symbols=';
   var callback = '&callback=JSON_CALLBACK';
 
   $http({
     method: 'JSONP',
-    url: url + vm.top + callback
+    url: url + vm.topQuotes + callback
   }).success(function cb(res){
     vm.data = res.results;
     console.log(vm.data);
   });
+
+  $scope.$watch('search', function() {
+    vm.search = $scope.search;
+    searchQuote();
+  });
+
+  function searchQuote(){
+    $http({
+      method: 'JSONP',
+      url: url + vm.search + callback
+    }).success(function cb(res){
+
+      vm.data = res.results;
+      console.log(vm.data);
+    });
+  }
 }
